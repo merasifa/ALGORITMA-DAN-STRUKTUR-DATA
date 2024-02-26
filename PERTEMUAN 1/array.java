7edddb9462a448fe22f7abd727d385df3870de04
merasifa <merasifa@gmail.com>
2024-02-26 21:10:46 +0700

    update

--- PERTEMUAN 1/array.java
+++ PERTEMUAN 1/array.java
@@ -1,8 +1,29 @@
+import java.util.Scanner;
+
 public class array {
     
     public static void main(String[] args) {
-        String nim;
+        String namaMatkul;
+        int nilai;
+        Scanner input27 = new Scanner(System.in);
 
 
-    }
+        System.out.println("==============================");
+        System.out.println("Program Menghitung IP Semester");
+        System.out.println("==============================");
+
+        System.out.print("Masukkan nilai angka untuk MK Pancasila             :");
+        nilai = input27.nextInt();
+        System.out.print("Masukkan nilai angka untuk MK KTI                   :");
+        nilai = input27.nextInt();
+        System.out.print("Masukkan nilai angka untuk MK CTPS                  :");
+        System.out.print("Masukkan nilai angka untuk MK Matematika Dasar      :");
+        System.out.print("Masukkan nilai angka untuk MK Bahasa Inggris        :");
+        System.out.print("Masukkan nilai angka untuk MK Dasar Pemograman      :");
+        System.out.print("Masukkan nilai angka untuk MK Prak Dasar Pemograman :");
+        System.out.print("Masukkan nilai angka untuk MK K3                    :");
+
+
+
+    }   
 }
