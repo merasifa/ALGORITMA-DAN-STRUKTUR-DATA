a68fcfbfe71ef26eba2803b3f0f1f08b5050c159
merasifa <merasifa@gmail.com>
2024-02-26 14:08:43 +0700

    update

--- PERTEMUAN 1/nilaiAkhir.java
+++ PERTEMUAN 1/nilaiAkhir.java
@@ -11,8 +11,8 @@ public class nilaiAkhir {
         int akhir = 0;
         char status;
 
-        System.out.println(" Program Menghitung Nilai Akhir");
-        System.out.println(" ==============================");
+        System.out.println("Program Menghitung Nilai Akhir");
+        System.out.println("==============================");
         System.out.print("Masukkan Nilai Tugas: ");
         tugas = inputNilai.nextInt();
         System.out.print("Masukkan Nilai Kuis : ");
@@ -21,38 +21,44 @@ public class nilaiAkhir {
         uts = inputNilai.nextInt();
         System.out.print("Masukkan Nilai UAS  : ");
         uas = inputNilai.nextInt();
-        System.out.println(" ==============================");
+        System.out.println("==============================");
         
         if (tugas <= 100 && kuis <=100 && uts <= 100 && uas <= 100) {
              akhir =(tugas * 20 / 100) + ( kuis  * 20 / 100) + ( uts  * 30 / 100) + ( uas  * 40 / 100);
 
             System.out.println("Nilai Akhir : " + akhir);
+
+            if (akhir >= 80 && akhir <=100) {
+                System.out.println( "Nilai Huruf : A");
+            } else if(akhir >= 73 && akhir <=80) {
+                System.out.println(" Nilai Akhir : B+ ");
+            } else if(akhir >= 65 && akhir <=73) {
+                System.out.println("Nilai Akhir : B ");
+            } else if(akhir >= 60 && akhir <=65) {
+                System.out.println("Nilai Akhir : C+ ");
+            }else if(akhir >= 50 && akhir <=60) {
+                System.out.println("Nilai Akhir : C  ");
+            }else if(akhir >= 39 && akhir <=50) {
+                System.out.println("Nilai Akhir : D ");
+            }else{
+                System.out.println("Nilai Akhir : E ");
+            }
+    
+            System.out.println("==============================");
+    
+            if (akhir<= 100 && akhir >= 50 ) {
+                System.out.println("LULUS");
+            } else if (akhir<= 50 && akhir >=0){
+                System.out.println("TIDAK LULUS");
+            }
+
+            System.out.println("==============================");
+
         } else {
             System.out.println("Nilai yang anda masukkan tidak valid");
         }
 
-        if (akhir >= 80 && akhir <=100) {
-            System.out.println( "Nilai Huruf : A");
-        } else if(akhir >= 73 && akhir <=80) {
-            System.out.println(" Nilai Akhir : B+ ");
-        } else if(akhir >= 65 && akhir <=73) {
-            System.out.println("Nilai Akhir : B ");
-        } else if(akhir >= 60 && akhir <=65) {
-            System.out.println("Nilai Akhir : C+ ");
-        }else if(akhir >= 50 && akhir <=60) {
-            System.out.println("Nilai Akhir : C  ");
-        }else if(akhir >= 39 && akhir <=50) {
-            System.out.println("Nilai Akhir : D ");
-        }else{
-            System.out.println("Nilai Akhir : E ");
-        }
+        System.out.println("==============================");
 
-        System.out.println( "============");
-
-        if (akhir<= 100 && akhir >= 50 ) {
-            System.out.println("LULUS");
-        } else if (akhir<= 50 && akhir >=0){
-            System.out.println("TIDAK LULUS");
-        }
     }
 }
