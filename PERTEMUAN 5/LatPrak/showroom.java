af1fea72effa65dbe353e8e0442e35fc305427f1
merasifa <merasifa@gmail.com>
2024-03-17 22:47:16 +0700

    update

--- PERTEMUAN 5/LatPrak/showroom.java
+++ PERTEMUAN 5/LatPrak/showroom.java
@@ -1,6 +1,58 @@
 public class showroom {
-    String []merk ={"BMW",""};
-    int tahun, topA, topP;
+    public String merk, tipe;
+    public int tahun, topA, topP;
 
 
+    public showroom(String merk, String tipe, int tahun, int topA, int topP){
+        this.merk = merk;
+        this.tipe = tipe;
+        this.tahun = tahun;
+        this.topA = topA;
+        this.topP = topP;
+    }
+
+    int topA(showroom[]list, int low, int high){
+        if (low==high){
+            return list[low].topA;
+        }
+        int mid = (low+high)/2;
+        int max1 = topA(list, low, mid);
+        int max2 = topA(list, mid+1, high);
+
+        if (max1 > max2){
+            return max1;
+        } else {
+            return max2;
+        }
+    }
+
+    int minA(showroom[]list, int low, int high){
+        if (low==high){
+            return list[low].topA;
+        }
+        int mid = (low+high)/2;
+        int min1 = minA(list, low, mid);
+        int min2 = minA(list, mid+1, high);
+
+        if (min1 <= min2){
+            return min1;
+        } else {
+            return min2;
+        }
+    }
+
+    double rata(showroom[]list){
+        int total = 0;
+        int hasil = 0;
+
+        for(int i = 0; i <list.length; i++){
+            total = total+ list[i].topP;
+            hasil++;
+        }
+
+        if (hasil == 0){
+            return 0.0;
+        }
+        return(double) total/hasil;
+    }
 }
--- PERTEMUAN 5/LatPrak/showroomMain.java
+++ PERTEMUAN 5/LatPrak/showroomMain.java
@@ -1,5 +1,23 @@
 public class showroomMain {
     public static void main(String[] args) {
-        
+            showroom[] list = {
+                new showroom("BMW", "M2 Coupe", 2016, 6816, 728),
+                new showroom("Ford", "Fiesta ST", 2014, 3921, 575),
+                new showroom("Nissan", "370Z", 2009, 4360, 657),
+                new showroom("Subaru", "BRZ", 2014, 4058, 609),
+                new showroom("Subaru", "Impreza WRX STI", 2013, 6255, 703),
+                new showroom("Toyota", "AE86 Trueno", 1986, 3700, 553),
+                new showroom("Toyota", "86/GT86", 2014, 4180, 609),
+                new showroom("Volkswagen", "Golf GTI", 2014, 4180, 631)
+            };
+
+        int topA = list[0].topA(list, 0, list.length -1);
+        int minA = list[0].minA(list, 0, list.length -1);
+
+
+        double rata = list[0].rata(list);
+        System.out.println("TOP ACCELERATION TERTINGGI: "+topA);
+        System.out.println("TOP ACCELERATION TERENDAH: "+minA);
+        System.out.println("RATA RATA TOP POWER: "+rata);
     }
 }
